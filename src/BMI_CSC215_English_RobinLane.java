cacabc734b4d9ab2f3032595802f5ef65a2b92b5
BuildTools <unconfigured@null.spigotmc.org>
2025-02-23 15:41:53 -0800

    Added method for gathering the primary user input
    Also imported scanner

--- src/BMI_CSC215_English_RobinLane.java
+++ src/BMI_CSC215_English_RobinLane.java
@@ -12,9 +12,15 @@
  *
  *********************************************************************************/
 
+import java.util.Scanner;
 
 public class BMI_CSC215_English_RobinLane
 {
+    static String name;
+    static int heightFt;
+    static int heightIn;
+    static int weightLb;
+
     static Scanner input = new Scanner(System.in);
 
     /* Main method only calls each section method in this order:
@@ -31,6 +37,8 @@ public class BMI_CSC215_English_RobinLane
     public static void main(String[] args)
     {
         displayWelcome();
+
+        gatherPrimaryInput();
     }
 
     static void displayWelcome()
@@ -40,5 +48,18 @@ public class BMI_CSC215_English_RobinLane
         System.out.println("--            BODY MASS INDEX (BMI) Computation, CSC 215, English version");
         System.out.println("--                                                                   by Robin Lane");
         System.out.println("----------------------------------------------------------------------------------------------");
+    }
+
+    static void gatherPrimaryInput()
+    {
+        System.out.print("Please enter your full name: ");
+        name = input.nextLine();
+
+        System.out.printf("Please enter height in feet and inches for %s: ", name);
+        heightFt = input.nextInt();
+        heightIn = input.nextInt();
+
+        System.out.printf("Please enter weight in pounds for %s: ", name);
+        weightLb = input.nextInt();
     }
 }
